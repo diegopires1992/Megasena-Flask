11c54a934afe6ae54e7f91e4ceb01ff557de51f2
DiegoPires <programadorpires@gmail.com>
2021-04-14 09:06:23 -0300

    feat:feito o update do usuario

--- app/views/user_views.py
+++ app/views/user_views.py
@@ -2,6 +2,8 @@ from http import HTTPStatus
 from flask import Blueprint,request,current_app
 from datetime import timedelta
 
+from flask.globals import session
+
 from flask_jwt_extended import create_access_token,create_refresh_token,jwt_required,get_jwt_identity
 from app.models.users_model import UsersModel
 
@@ -16,12 +18,17 @@ def create_user():
         session = current_app.db.session
         
         body = request.get_json()
-        if not body:
-            return {"msg": "Verifique o body da requisição"},HTTPStatus.BAD_REQUEST
 
         name = body.get("name")
         email = body.get("email")
         password = body.get("password")
+
+        found_user: UsersModel = UsersModel.query.filter_by(email=email).first()
+        if found_user:
+            return {"msg": "Usuario já cadastrado"},HTTPStatus.BAD_REQUEST
+
+        if not body:
+            return {"msg": "Verifique o body da requisição"},HTTPStatus.BAD_REQUEST
 
         new_user = UsersModel(name=name,email=email)
         new_user.password = password
@@ -29,32 +36,66 @@ def create_user():
         session.commit()
 
         access_token = create_access_token(identity=new_user.id,expires_delta=timedelta(days=7))
-        fresh_token = create_access_token(identity=new_user.id,expires_delta=timedelta(days=14))
         return {
                 "user":{
                     "name":new_user.name,
                     "email":new_user.email,
-                    "access_token":access_token,
-                    "fresh_token":fresh_token
+                    "access_token":access_token
                     }
                 },HTTPStatus.CREATED
 
-    except KeyError:
-        return {"msg": "Verifique o body da requisição"},HTTPStatus.BAD_REQUEST
+    except Exception:
+        return {"msg": "Erro ao cadastrar"},HTTPStatus.BAD_REQUEST
     
 @bp_user.route("/login",methods=['POST'])
 def login():
-    body = request.get_json()
-    email = body.get('email')
-    password = body.get('password')
+    try:
+        body = request.get_json()
+        email = body.get('email')
+        password = body.get('password')
 
-    found_user: UsersModel = UsersModel.query.filter_by(email=email).first()
+        found_user: UsersModel = UsersModel.query.filter_by(email=email).first()
 
-    if not found_user or not found_user.check_password(password):
-        return {"msg": "Usuario ou senha incorreto"},HTTPStatus.BAD_REQUEST
+        if not found_user or not found_user.check_password(password):
+            return {"msg": "Usuario ou senha incorreto"},HTTPStatus.BAD_REQUEST
 
-    access_token = create_access_token(identity=found_user.id,expires_delta=timedelta(days=7))
-    fresh_token = create_access_token(identity=found_user.id,expires_delta=timedelta(days=14))
+        access_token = create_access_token(identity=found_user.id,expires_delta=timedelta(days=7))
 
-    return{"access_token":access_token,"fresh_token":fresh_token}
+        return{"access_token":access_token}
+    except Exception:
+        return {"msg": "Erro ao fazer login"},HTTPStatus.BAD_REQUEST
+
+@bp_user.route("/",methods=["PUT"])
+@jwt_required()
+def update_user():
+    try:
+        session = current_app.db.session
+        body = request.get_json()
+        user_id = get_jwt_identity()
+
+        body = request.get_json()
+        if not body:
+            return {"msg": "Verifique o body da requisição"},HTTPStatus.BAD_REQUEST
+            
+        name = body.get("name")
+        email = body.get("email")
+        password = body.get("password")
+
+        found_user: UsersModel = UsersModel.query.get(user_id)
+        found_user.name = name
+        found_user.email = email
+        found_user.password = password
+
+        session.add(found_user)
+        session.commit()
+
+        return{
+            "user":{
+                "name":found_user.name,
+                "email":found_user.email
+            }
+        },HTTPStatus.OK
+
+    except Exception:
+        return {"msg": "Erro ao fazer update no usuario"},HTTPStatus.BAD_REQUEST
 
